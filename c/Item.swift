21ae226c5831eea95bb2a936c03a256474f97f47
Prajz <d69praj@gmail.com>
2024-01-19 16:31:01 +0000

    Initial Commit

---
+++ c/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  c
+//
+//  Created by Dev Prajapati on 19/01/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
